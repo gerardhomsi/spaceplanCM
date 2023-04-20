aa523459eebcf56686306da020b23fe2f75b9863
Gerard Homsi <gerard.homsi@gmail.com>
2023-04-20 16:22:50 -0500

    some modifications

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import './hero.css'
+import React, { useState } from 'react';
+import './hero.css';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+
+
+const images = [
+  "https://via.placeholder.com/400x200?text=Image+1",
+  "https://via.placeholder.com/400x200?text=Image+2",
+  "https://via.placeholder.com/400x200?text=Image+3",
+  "https://via.placeholder.com/400x200?text=Image+4",
+];
 
 const Hero = () => {
-  return (
-    <>
-    <div className='hero'>
-        <p>SpacePlan Construction and Management LLC is an experienced design and construction company that has been operating since 2007. Over the past three decades, we have successfully completed numerous commercial and residential projects throughout the Houston area. Our turn-key services include tenant improvements, new construction, and residential remodels, where we provide preliminary design, budgetary cost estimates, construction drawings, permit services, and construction management.</p>
-    </div>
-    <div>
-        <p>
-        somethingsoadjoasjdsodaskds
-        </p>
-    </div>
-    </>
-  )
-}
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  
+    const previousImage = () => {
+      setCurrentImageIndex(
+        currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1
+      );
+    };
+  
+    const nextImage = () => {
+      setCurrentImageIndex(
+        currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1
+      );
+    };
+  
+    return (
+      <>
+        <div className='hero'>
+          <div className="slider-container">
+            <img
+              className='slider-image'
+              src={images[currentImageIndex]}
+              alt='Slider'
+            />
+            <div className='slider-arrows'>
+              <div className='slider-arrow-left' onClick={previousImage}>
+                <FaArrowAltCircleLeft />
+              </div>
+              <div className='slider-arrow-right' onClick={nextImage}>
+                <FaArrowAltCircleRight />
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
 
-export default Hero
+  export default Hero;
